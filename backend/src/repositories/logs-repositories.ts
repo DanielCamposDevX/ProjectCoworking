836b29b30d57334ff8d85fe2a2f9cccec968220b
DanielCamposDevX <daniel.campos@devio.com.br>
2024-09-21 18:01:02 -0300

    backend.feature(socketStarter): created socket connection & room for user notifications &&& fix(swagger docs)

--- backend/src/repositories/logs-repositories.ts
+++ backend/src/repositories/logs-repositories.ts
@@ -1,14 +1,31 @@
-import { prisma } from '../config/database';
+import { prisma } from '../config/database.js';
+import { io } from '../server.js';
 
 async function createLog(userId: number, projectId: number, action: string) {
-   return await prisma.logs.create({
+   const newLog = await prisma.logs.create({
       data: {
          acao: action,
          data: new Date(),
          projetoId: projectId,
          usuarioId: userId,
       },
+      select: {
+         projeto: {
+            select: {
+               usuarios: {
+                  select: {
+                     id: true,
+                  },
+               },
+            },
+         },
+      },
    });
+   newLog.projeto.usuarios.forEach((user) =>
+      io.to(user.id.toString()).emit('log', newLog),
+   );
+
+   return newLog;
 }
 
 export const logsRepositories = { createLog };
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,32 @@
+import { createServer } from 'http';
+import { Server as SocketServer } from 'socket.io';
 import app, { init } from './app.js';
 
 const port = +process.env.PORT || 4000;
+const httpServer = createServer(app);
+const io = new SocketServer(httpServer, { cors: { origin: '*' } });
+
+io.on('connection', (socket) => {
+   console.log('Um cliente conectado:', socket.id);
+   socket.emit('log', 'Você está conectado!');
+   socket.on('joinRoom', async (room) => {
+      console.log('cliente entrou na sala:', room);
+      await socket.join(`${room}`);
+   });
+
+   socket.on('disconnect', () => {
+      console.log('Cliente desconectado:', socket.id);
+   });
+});
 
 init()
    .then(() => {
-      app.listen(port, () => {
+      httpServer.listen(port, () => {
          console.log(`Server is listening on port ${port}.`);
       });
    })
    .catch((err) => {
       console.error(err);
    });
+
+export { io };
